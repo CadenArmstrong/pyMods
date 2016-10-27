395b780bbefe6179869f74f61dd7b5d5870bde26
CadenArmstrong <cadenarmstrong@gmail.com>
2016-10-27 16:50:36 +0000

    Initial setup

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # pyMods
 Tool to modify islandora MODS records using CRUD
+
+1. Get the pids
+2. Gets the MODS
+3. Modify the MODS
+4. Push the MODS
---
+++ pyMods.py
@@ -0,0 +1,24 @@
+# Author: Caden Armstrong
+# PyMods
+# A python tool for working with islandora CRUD and MODS
+
+def fetch_pids():
+    """ Fetch the pids of stuff"""
+
+    pass
+
+
+def fetch_mods(pids):
+    """ Fetch the mods files given a list of pids"""
+
+    pass
+
+def modify_mods(mods_files):
+    """ Modifies the mods files"""
+
+    pass
+
+def push_mods(mods_files):
+    """ Pushes modified mods files"""
+
+    pass
